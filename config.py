04a29429d0cb6555f55ab547bbe5030b7640914f
NiyazProgrammer <niazr90@mail.ru>
2025-03-04 16:09:32 +0300

    Added the basic architecture. Created files for the API

---
+++ config.py
@@ -0,0 +1,8 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+BITGET_API_KEY = os.getenv("BITGET_API_KEY")
+BITGET_SECRET_KEY = os.getenv("BITGET_SECRET_KEY")
+BITGET_PASSPHRASE = os.getenv("BITGET_PASSPHRASE")
---
+++ main.py
@@ -0,0 +1,7 @@
+from utils.logging_setup import setup_logger
+
+def main():
+    setup_logger()
+
+if __name__ == "__main__":
+    main()
